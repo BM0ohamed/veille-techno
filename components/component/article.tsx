4d4e4776067d30317a94c0fbba6817d8df5d0742
Abderrahmane Mouzoune <abderrahmane_mouzoune@carrefour.com>
2024-01-22 11:10:50 +0100

    ajout d'une liste d'article

---
+++ components/component/article.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import Link from "next/link"
+import { ArrowRight } from "lucide-react"
+
+type ArticleProps = {
+  nom: string
+  description: string
+  url: string
+}
+
+export default function Article({ nom, description, url }: ArticleProps) {
+  return (
+    <Card className="px-2 py-2">
+      <CardHeader>
+        <h3 className="text-lg font-semibold">{nom}</h3>
+      </CardHeader>
+      <CardContent>
+        <p className="text-gray-500">{description}</p>
+      </CardContent>
+      <CardFooter>
+        <Link
+          className="text-indigo-600 hover:text-indigo-500 flex items-center"
+          href={url}
+        >
+          Lien vers article{" "}
+          <small className="pl-2">
+            <ArrowRight />
+          </small>
+        </Link>
+      </CardFooter>
+    </Card>
+  )
+}
